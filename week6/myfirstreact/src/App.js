73c2c478f378e374200b0a46e72f6fd58407d8c7
Anusha <anushabalaji2005@gmail.com>
2025-07-22 21:29:44 +0530

    Added week 6 Hands-On Solutions

---
+++ week6/myfirstreact/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  const styles = {
+    textAlign: 'center',
+    marginTop: '30px',
+    fontFamily: 'Arial, sans-serif',
+  };
+
+  return (
+    <div>
+      <h1 style={styles}>Welcome to the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
